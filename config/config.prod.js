3d161cbb3fdf1ce4af28a52cdbe891cbd8896a17
chuck <121477964@qq.com>
2019-07-04 16:18:14 +0800

    将部署的配置文件加上去了，同时增加了部署的包和命令

---
+++ config/config.prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+  keys: 'chuckboyproduction',
+  cluster: {
+    listen: {
+      port: 3000,
+      hostname: '127.0.0.1',
+      // path: '/var/run/egg.sock',
+    }
+  }
+}
+
